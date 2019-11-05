9a35acc61047a46736d08c5f0d4410bc073d6d76
max <kibemax2000@gmail.com>
2019-11-05 16:31:17 +0300

    fff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header">{{ __('Tenant ') }}</div>
+                        <div class="card-header">{{ __('Tenant Login') }}</div>
 
                         <div class="card-body">
                             <form method="POST" action="{{ route('login') }}">
